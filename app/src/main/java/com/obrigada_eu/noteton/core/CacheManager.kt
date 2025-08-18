180f9956ce0391b6ee2304a3a3c78658252cf1b4
lunatico <efanya@gmail.com>
2025-08-18 16:37:14 +0100

    build: update dependencies, Gradle plugin, and use-site target

--- app/src/main/java/com/obrigada_eu/noteton/core/CacheManager.kt
+++ app/src/main/java/com/obrigada_eu/noteton/core/CacheManager.kt
@@ -6,7 +6,7 @@ import androidx.core.net.toUri
 import dagger.hilt.android.qualifiers.ApplicationContext
 import java.io.File
 
-class CacheManager(@ApplicationContext private val context: Context) {
+class CacheManager(@param:ApplicationContext private val context: Context) {
 
     val tempPhotoFile: File by lazy { context.cacheDir.resolve("tempPhotoFile.jpg") }
     val tempPhotoUri: Uri by lazy { tempPhotoFile.toUri() }
--- app/src/main/java/com/obrigada_eu/noteton/presentation/root/NotetonScreen.kt
+++ app/src/main/java/com/obrigada_eu/noteton/presentation/root/NotetonScreen.kt
@@ -3,7 +3,7 @@ package com.obrigada_eu.noteton.presentation.root
 import androidx.annotation.StringRes
 import com.obrigada_eu.noteton.R
 
-enum class NotetonScreen(@StringRes val title: Int) {
+enum class NotetonScreen(@param:StringRes val title: Int) {
     NotesList(title = R.string.notes_list),
     AddNote(title = R.string.add_note),
     CameraPreview(title = R.string.camera_preview)
